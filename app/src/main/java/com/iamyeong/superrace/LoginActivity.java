07410c4788f61879082937be3c72584f26f6c3ce
IamYeong <wjdrhkddud2@gmail.com>
2021-05-10 17:08:24 +0900

    LoginActivity xml file edit

--- app/src/main/java/com/iamyeong/superrace/LoginActivity.java
+++ app/src/main/java/com/iamyeong/superrace/LoginActivity.java
@@ -2,17 +2,66 @@ package com.iamyeong.superrace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText et_id, et_pw;
+    private Button btn_sign_in, btn_sign_up;
+    private ImageView img_kakao, img_facebook, img_naver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        allFindViewById();
+        allSetOnClickListener(LoginActivity.this);
 
 
+
+
+
+    }
+
+    private void allFindViewById() {
+
+        et_id = findViewById(R.id.et_login_id);
+        et_pw = findViewById(R.id.et_login_pw);
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
+
+        img_kakao = findViewById(R.id.img_kakao);
+        img_facebook = findViewById(R.id.img_facebook);
+        img_naver = findViewById(R.id.img_naver);
+
+    }
+
+    private void allSetOnClickListener(View.OnClickListener listener) {
+
+        btn_sign_in.setOnClickListener(listener);
+        btn_sign_up.setOnClickListener(listener);
+
+        img_kakao.setOnClickListener(listener);
+        img_facebook.setOnClickListener(listener);
+        img_naver.setOnClickListener(listener);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+
+
+        }
 
     }
 }
